31c53a3bed2b9fe88651e8346c7ff3b2bc15566d
Semih Biygit <semihbiygit@gmail.com>
2022-11-03 14:41:27 +0300

    api-gateway & config yapılandırma sürüyor...

--- config-server/src/main/java/com/bilgeadam/ConfigServerApplication.java
+++ config-server/src/main/java/com/bilgeadam/ConfigServerApplication.java
@@ -1,7 +1,13 @@
 package com.bilgeadam;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
--- user-service/src/main/java/com/bilgeadam/UserServiceApplication.java
+++ user-service/src/main/java/com/bilgeadam/UserServiceApplication.java
@@ -1,7 +1,11 @@
 package com.bilgeadam;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
